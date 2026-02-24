2c39ce404d76ccd031c24f3e13a598ceaae92fc6
Aaron Spiteri <azzmosphere@gmail.com>
2026-02-24 16:03:09 +1100

    modified launch config to suite modified motor controller

--- launch/rr_mousebot.launch.py
+++ launch/rr_mousebot.launch.py
@@ -28,38 +28,37 @@ def generate_launch_description():
     """launch composable nodes"""
 
     # Include LDLidar launch
-    ldlidar_launch = IncludeLaunchDescription(
-        launch_description_source=PythonLaunchDescriptionSource([
-            get_package_share_directory('ldlidar_node'),
-            '/launch/ldlidar_bringup.launch.py'
-        ]),
-        launch_arguments={
-            'node_name': 'ldlidar_node'
-        }.items()
-    )
+    # ldlidar_launch = IncludeLaunchDescription(
+    #     launch_description_source=PythonLaunchDescriptionSource([
+    #         get_package_share_directory('ldlidar_node'),
+    #         '/launch/ldlidar_bringup.launch.py'
+    #     ]),
+    #     launch_arguments={
+    #         'node_name': 'ldlidar_node'
+    #     }.items()
+    # )
 
     return launch.LaunchDescription([
-
-        Node(
-            package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='lifecycle_manager_navigation',
-            output='screen',
-            parameters=[{
-                'autostart': True,
-                'bond_timeout': 0.0,
-                'node_names': [
-                    '/driver/serial_bridge_node',
-                    '/driver/udp_receiver_node',
-                    '/driver/udp_sender_node',
-                    '/sensor/rr_imu_action_node',
-                    'ldlidar_node',
-                ]
-            }],
-        ),
+        # Node(
+        #     package='nav2_lifecycle_manager',
+        #     executable='lifecycle_manager',
+        #     name='lifecycle_manager_navigation',
+        #     output='screen',
+        #     parameters=[{
+        #         'autostart': True,
+        #         'bond_timeout': 0.0,
+        #         'node_names': [
+        #             '/driver/serial_bridge_node',
+        #             '/driver/udp_receiver_node',
+        #             '/driver/udp_sender_node',
+        #             '/sensor/rr_imu_action_node',
+        #             'ldlidar_node',
+        #         ]
+        #     }],
+        # ),
 
         # Include LDLidar bringup (lifecycle node)
-        ldlidar_launch,
+        # ldlidar_launch,
 
         # Low level communications, serial, and UDP, or bridges to flight controller.
         ComposableNodeContainer(
@@ -67,80 +66,73 @@ def generate_launch_description():
             namespace='driver',
             package='rclcpp_components',
             executable='component_container_mt',
+            arguments=['--ros-args', '--log-level', 'info'],
             composable_node_descriptions=[
-                # Note that this is a lifecycle node within this version
+
                 ComposableNode(
-                    package='udp_driver',
-                    plugin='drivers::udp_driver::UdpReceiverNode',
-                    namespace='driver',
-                    name='udp_receiver_node',
+                    package='rr_motor_controller',
+                    plugin='rr_motor_controller::RrMotorController',
+                    name='motor_left',
+                    namespace='',
                     parameters=[{
-                        "ip": '0.0.0.0',
-                        "port": 57410,
+                        'motor_pos': 0,
+                        'ppr': 8,
+                        'wheel_radius': 20,
+                        'transport_plugin': 'rr_gpio_pi4b_pigpio_plugin::RrGpioPi4BPigpioPlugin',
+                        'pwm_pin': 18,
+                        'dir_pin': 23,
+                        'pwm_freq': 1000,
+                        'encoder_pin': 9,
+                        'encoder_timeout': 500000,
                     }],
-                    extra_arguments=[{'use_intra_process_comms': True}],
                 ),
 
-                # Note that this is a lifecycle node within this version
-                ComposableNode(
-                    package='udp_driver',
-                    plugin='drivers::udp_driver::UdpSenderNode',
-                    namespace='driver',
-                    name='udp_sender_node',
-                    parameters=[{
-                        # transport_driver does not natively support
-                        # SO_BROADCAST option, so change IP Address as
-                        # appropriate
-                        "ip": '192.168.2.8',
-                        "port": 57410,
-                    }],
-                    extra_arguments=[{'use_intra_process_comms': True}],
-                ),
+                # ComposableNode(
+                #     package='rr_motor_controller',
+                #     plugin='rr_motor_controller::RrMotorControllerNode',
+                #     name='motor_right',
+                #     namespace='',
+                #     parameters=[{
+                #         'motor_pos': 1,
+                #         'ppr': 8,
+                #         'wheel_radius': 20,
+                #         'transport_plugin': 'rr_gpio_pi4b_pigpio_plugin::RrGpioPi4BPigpioPlugin',
+                #         'pwm_pin': 19,
+                #         'dir_pin': 24,
+                #         'pwm_freq': 1000,
+                #         'encoder_pin': 8,
+                #         'encoder_timeout': 500000,
+                #     }],
+                # ),
 
                 # Note that this is a lifecycle node within this version
-                #
-                # Note that bonding needs to be added to this node, may
-                # be an idea to add it separately and wrapper for the lifecycle
-                # node.
-                ComposableNode(
-                    package='serial_driver',
-                    plugin='drivers::serial_driver::SerialBridgeNode',
-                    name='serial_bridge_node',
-                    namespace='driver',
-                    parameters=[{
-                        "device_name": '/dev/ttyACM0',
-                        "baud_rate": 115200,
-                        "flow_control": "none",
-                        "parity": "none",
-                        "stop_bits": "1"
-                    }],
-                    extra_arguments=[{'use_intra_process_comms': True}],
-                ),
+                # ComposableNode(
+                #     package='udp_driver',
+                #     plugin='drivers::udp_driver::UdpReceiverNode',
+                #     namespace='driver',
+                #     name='udp_receiver_node',
+                #     parameters=[{
+                #         "ip": '0.0.0.0',
+                #         "port": 57410,
+                #     }],
+                #     extra_arguments=[{'use_intra_process_comms': True}],
+                # ),
+
+                # # Note that this is a lifecycle node within this version
+                # ComposableNode(
+                #     package='udp_driver',
+                #     plugin='drivers::udp_driver::UdpSenderNode',
+                #     namespace='driver',
+                #     name='udp_sender_node',
+                #     parameters=[{
+                #         # transport_driver does not natively support
+                #         # SO_BROADCAST option, so change IP Address as
+                #         # appropriate
+                #         "ip": '192.168.1.20',
+                #         "port": 57410,
+                #     }],
+                #     extra_arguments=[{'use_intra_process_comms': True}],
+                # ),
             ],
-        ),
-
-        # Nodes in this container are one level away from raw hardware, but are still low level
-        # they are expected to work extremely fast.
-        #
-        # This node should be bonded, this needs to be implemented in next 
-        # iteration of the code.
-        ComposableNodeContainer(
-            name='sensor_nodes',
-            namespace='sensor',
-            package='rclcpp_components',
-            executable='component_container_mt',
-            arguments=['--ros-args', '--log-level', 'DEBUG'],
-            composable_node_descriptions=[                
-                ComposableNode(
-                    package="rr_imu_action",
-                    namespace='sensor',
-                    plugin="rr_imu_action::RrImuActionNode",
-                    name="rr_imu_action_node",
-                    parameters=[{'transport_plugin': 'rr_common_plugins::rr_serial_plugins::ImuActionSerialPlugin'}],
-                    extra_arguments=[
-                        {'use_intra_process_comms': True},
-                    ],
-                )
-            ]
         ),
     ])
